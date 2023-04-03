50520e28d76f54e775f83c1df3ae0fcaa57f5d37
Beren <k33250@naver.com>
2023-04-03 21:16:18 +0900

    ItemsLists Updata fillter list

--- src/itemsPrice/ItemsLists.js
+++ src/itemsPrice/ItemsLists.js
@@ -1,17 +1,45 @@
 import { useEffect, useState } from "react"
-import { organizeCategorie } from "../utils/Util";
+import { ItemNameDataFilter, organizeCategorie } from "../utils/Util";
 
 export default function ItemsLists({ filterData }) {
     const [nameTag, setNameTag] = useState([]);
+    const [list, setList] = useState([]);
 
     useEffect(() =>{
         setNameTag(organizeCategorie(filterData, 'A_NAME'));
-
-        if (nameTag.length > 0) {
-            
-        }
     }, [filterData])
-    console.log(nameTag)
+    
+    useEffect(() => {    
+        // 데이터 리스트업: 최저가, 최고가, 평균가 계산
+        if (nameTag.length > 0) {
+            const lists = nameTag.map(name => {
+                const items = ItemNameDataFilter(filterData, name);
+                const sort = items.sort((a,b) => parseInt(a.A_PRICE) - parseInt(b.A_PRICE));
+                let medium;
+
+                if (sort.length === 1) {
+                    medium = sort[0].A_PRICE;
+                } else {
+                    medium = items.reduce((acc, item) => {
+                        return acc + parseInt(item.A_PRICE)
+                    }, 0) / sort.length
+                }
+
+                const itemList = {
+                    name: name,
+                    lowest: sort[0].A_PRICE,
+                    highest: sort[sort.length - 1].A_PRICE,
+                    medium: medium
+                }
+                
+                return itemList
+            })
+
+            setList([...lists])
+        }
+        
+    }, [nameTag])
+    console.log(list)
     return(
         <section className="xl:ml-72">
             <div className="border py-4 max-w-6xl mx-auto xl:px-0 px-4">
