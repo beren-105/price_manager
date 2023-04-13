35bdfa13308c9c9e4cdd929b9650913f82aee8c6
Beren <k33250@naver.com>
2023-04-13 19:38:23 +0900

    LocalChart Remove chart

--- src/localPrice/LocalChart.js
+++ src/localPrice/LocalChart.js
@@ -1,67 +1,40 @@
 import { useEffect, useState } from "react";
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend
-  } from "recharts";
-  import LocalList from "./LocalList";
+import LocalList from "./LocalList";
 
 
 export default function LocalChart({ chartData, listData }) {
-    const [data, setData] = useState(null);
-
-
-    useEffect(() => {
-        // 데이터 정리
-        setData([{
-            name : chartData.name,
-            최저가 : chartData.lowest,
-            최고가 : chartData.highest,
-            평균가 : chartData.medium
-        }])
-
-    }, [chartData])
 
     console.log(listData)
 
 
     return (
-        <article className="flex flex-col items-center border p-4 rounded-xl">
+        <article className="flex flex-col items-center border p-4 rounded-xl basis-1/3">
             <div className='w-full'>
-                <h3 className='mt-2 mb-6 text-lg'>{chartData.name}</h3>
+                <h3 className='mt-2 mb-6 text-lg font-bold text-purple-500'>{chartData.name}</h3>
             </div>
+
             <div className='mb-4'>
-                <BarChart
-                width={320}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5
-                }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="최저가" fill="#c4b5fd" />
-                <Bar dataKey="평균가" fill="#a855f7" />
-                <Bar dataKey="최고가" fill="#f9a8d4" />
-            </BarChart>
+                <article>
+                    <p>평균가</p>
+                    <p>{chartData.medium}원</p>
+                </article>
+                <article>
+                    <p>최고가<span>{chartData.highest}원</span></p>
+                </article>
+                <article>
+                    <p>최저가<span>{chartData.lowest}원</span></p>
+                </article>
             </div>
-                {listData.map((data, index) => (
-                    <LocalList
-                        key={index}
-                        data={data}
-                    />
-                ))}
-            <div className='text-xl'>
+
+            <div className=''>
+                <ul>
+                    {listData.map((data, index) => (
+                        <LocalList
+                            key={index}
+                            listData={data}
+                        />
+                    ))}
+                </ul>
             </div>
         </article>
     )
--- src/localPrice/LocalList.js
+++ src/localPrice/LocalList.js
@@ -1,3 +1,10 @@
-export default function LocalList() {
+export default function LocalList({ listData }) {
     
+    return (
+        <li className="flex gap-4 justify-center">
+            <p>{listData.M_NAME}</p>
+            <p>{listData.A_PRICE}원</p>
+            <p>{listData.M_TYPE_NAME}</p>
+        </li>
+    )
 }
